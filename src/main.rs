2d5f15d89ca8d68946b14a81e771decf1d1e2742
Sakib <mohammad.sakib0194@gmail.com>
2022-04-04 02:56:10 +0600

    added xlsx reader

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 mod csv_reader;
-use csv_reader::read_csv;
+mod xlsx_reader;
+mod sub_func;
+
+use crate::sub_func::initiator;
 use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
+
 fn main() {
-    let csv_data = match read_csv("data.csv") {
+    
+    let data = match initiator() {
         Ok(data) => data,
         Err(e) => {
-            println!("Error reading CSV or data.csv not found. Error: {}", e);
-            let full_text = format!("Error reading CSV or data.csv not found. Error: {}", e);
+            println!("Error reading file data. Error: {}", e);
+            let full_text = format!("Error reading file data. Error: {}", e);
             let mut open = File::create("data.txt").expect("Error creating txt file");
             open.write_all(full_text.as_bytes()).expect("Error writing txt file");
             process::exit(1);
         }
     };
-
-    csv_data.print_write_average();
+    data.print_write_average();
 }
