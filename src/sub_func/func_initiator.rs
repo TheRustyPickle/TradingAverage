26187bbcca41a9439dbbe54ecd101f7004c4d4d0
Sakib <mohammad.sakib0194@gmail.com>
2022-04-05 20:48:47 +0600

    kucoin support and separate txt file for each

--- src/sub_func/func_initiator.rs
+++ src/sub_func/func_initiator.rs
@@ -1,41 +1,43 @@
-use crate::csv_reader::read_csv;
-use crate::xlsx_reader::read_xlsx;
+use crate::binance::csv_reader_binance::read_csv_bin;
+use crate::binance::xlsx_reader_binance::read_xlsx_bin;
+use crate::kucoin::csv_reader_kucoin::read_csv_kucoin;
 use super::order_data::OrderData;
 use std::fs;
 use std::process;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
-pub fn initiator() -> Result<OrderData, Box<dyn Error>> {
+pub fn file_initiator() -> HashMap<String, Result<OrderData, Box<dyn Error>>>{
+    let mut map = HashMap::new();
     let paths = fs::read_dir(".").unwrap();
-    let mut file_to_read = Some(String::new());
+
     for path in paths {
         let mut path = path.unwrap().path().display().to_string();
         path = path.replace("./", "");
-        if path == "data.csv" || path == "data.xlsx" {
-            file_to_read = Some(path);
+
+        match path.to_lowercase().as_ref() {
+            "binance.csv" => {
+                map.insert("binance_csv.txt".to_string(), read_csv_bin(&path));
+            },
+            "binance.xlsx" => {
+                map.insert("binance_xlsx.txt".to_string(), read_xlsx_bin(&path));
+            },
+            "kucoin.csv" => {
+                map.insert("kucoin.txt".to_string(), read_csv_kucoin(&path));
+            },
+            _ => {},
         }
     }
-    match file_to_read {
-        Some(file) => {
-            println!("Reading file {}", file);
-            if file == "data.csv" {
-                read_csv(&file)
-            }
-            else if file == "data.xlsx" {
-                read_xlsx(&file)
-            }
 
-            else {
-                process::exit(1)
-            }
-        },
-
-        None => {
-            println!("data.csv or data.xlsx not found in directory");
-            let mut open = File::create("data.txt").expect("Error creating txt file");
-            open.write_all("data.csv or data.xlsx not found in directory".as_bytes()).expect("Error writing txt file");
-            process::exit(1)},
+    if map.is_empty() {
+        println!("(filename).csv or (filename).xlsx not found in directory.\n\nAccepted filenames\n\nbinance.csv\nbinance.xlsx\nkucoin.csv");
+        let mut open = File::create("data.txt").expect("Failed to create data.txt");
+        open.write_all("(filename).csv or (filename).xlsx not found in directory.\n\nAccepted filenames\n\nbinance.csv\nbinance.xlsx\nkucoin.csv"
+                            .as_bytes()).expect("Failed to create data.txt");
+        process::exit(1)
     }
+
+    map
 }
