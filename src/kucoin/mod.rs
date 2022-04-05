9f8e8b9162ddab750dc009fa554b515ec5c97c92
Sakib <mohammad.sakib0194@gmail.com>
2022-04-05 20:45:45 +0600

    added kucoin order csv support

---
+++ src/kucoin/mod.rs
@@ -0,0 +1,3 @@
+pub mod csv_reader_kucoin;
+
+pub use csv_reader_kucoin::read_csv_kucoin;
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-mod csv_reader;
-mod xlsx_reader;
+mod binance;
+mod kucoin;
 mod sub_func;
 
-use crate::sub_func::initiator;
+use crate::sub_func::file_initiator;
 use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
 
 fn main() {
-    
-    let data = match initiator() {
-        Ok(data) => data,
-        Err(e) => {
-            println!("Error reading file data. Error: {}", e);
-            let full_text = format!("Error reading file data. Error: {}", e);
-            let mut open = File::create("data.txt").expect("Error creating txt file");
-            open.write_all(full_text.as_bytes()).expect("Error writing txt file");
-            process::exit(1);
+    let all_data = file_initiator();
+    for (key, value) in all_data.iter() {
+        match value {
+            Ok(v) => {
+                v.print_write_average(key)
+            }
+            Err(e) => {
+                println!("Error reading file data. Error: {}", e);
+                let full_text = format!("Error reading file data. Error: {}", e);
+                let mut open = File::create("data.txt").expect("Error creating txt file");
+                open.write_all(full_text.as_bytes()).expect("Error writing txt file");
+                process::exit(1);
+            }
         }
-    };
-    data.print_write_average();
+    }
 }
